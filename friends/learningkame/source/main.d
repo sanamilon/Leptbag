90710e6f612e6c3d49691f23e6aff24a447957f6
shikeee <srkt0417@icloud.com>
2017-10-25 19:03:45 +0900

    宙に浮くカメ

--- friends/learningkame/source/main.d
+++ friends/learningkame/source/main.d
@@ -36,7 +36,7 @@ class kame{
 
 		this.bodyInfo = info;
 
-		foreach(string name, params;bodyInfo.partParams){
+		foreach(string name, params; bodyInfo.partParams){
 				bodyInfo.partsGenerator[name] = new elementManager(bodyInfo.partParams[name].vertices, &createConvexHullShapeBody);
 		}
 
@@ -44,37 +44,59 @@ class kame{
 
 
 		foreach(string s, partsGen; bodyInfo.partsGenerator){
-
-			parts[s] = partsGen.generate(parameterPack(
-				param("position", bodyInfo.partParams[s].position + position),
-				param("scale",    bodyInfo.partParams[s].scale),
-				param("rotation", bodyInfo.partParams[s].rotation),
-				param("model",    bodyInfo.partParams[s].vertices),
-				param("mass",     bodyInfo.partParams[s].mass * bodyMass)));
+			switch(s){
+				case "shell":
+				parts[s] = partsGen.generate(parameterPack(
+					param("position", bodyInfo.partParams[s].position + position),
+					param("scale",    bodyInfo.partParams[s].scale),
+					param("rotation", bodyInfo.partParams[s].rotation),
+					param("model",    bodyInfo.partParams[s].vertices),
+					param("mass",     0.0f)));
+					break;
+				default:
+				parts[s] = partsGen.generate(parameterPack(
+					param("position", bodyInfo.partParams[s].position + position),
+					param("scale",    bodyInfo.partParams[s].scale),
+					param("rotation", bodyInfo.partParams[s].rotation),
+					param("model",    bodyInfo.partParams[s].vertices),
+					param("mass",     bodyInfo.partParams[s].mass * bodyMass)));
+					break;
+			}
 
 		}
 
+		writeln(bodyInfo);
 		foreach(string s, param; bodyInfo.g6dofParams){
-			g6dofs[s] = new generic6DofConstraint(
-				parts[bodyInfo.g6dofParams[s].object1Name],
-				parts[bodyInfo.g6dofParams[s].object2Name],
-				bodyInfo.g6dofParams[s].object1Position,
-				bodyInfo.g6dofParams[s].object2Position,
-				bodyInfo.g6dofParams[s].rotation);
+			elementNode obj1, obj2;
+			Vector3f pos1, pos2;
+			Quaternionf rotation;
+
+			obj1 = parts[bodyInfo.g6dofParams[s].object1Name];
+			obj2 = parts[bodyInfo.g6dofParams[s].object2Name];
+			pos1 = bodyInfo.g6dofParams[s].object1Position;
+			pos2 = bodyInfo.g6dofParams[s].object2Position;
+			rotation = bodyInfo.g6dofParams[s].rotation;
+
+			g6dofs[s] = new generic6DofConstraint(obj1, obj2, pos1, pos2, rotation);
 
 			switch(s){
-				case "Constraint.002", "Constraint.005", "Constraint.007", "Constraint.010":
-				g6dofs[s].setAngularLimit(bodyInfo.g6dofParams[s].angLimitLower, bodyInfo.g6dofParams[s].angLimitUpper);
+				case "leftback-shell":
+				g6dofs[s].setAngularLimit(zeroVec3, zeroVec3);
 				g6dofs[s].setLinearLimit (bodyInfo.g6dofParams[s].linLimitLower, bodyInfo.g6dofParams[s].linLimitUpper);
+				/*
 				g6dofs[s].setRotationalMotor(0);
 				g6dofs[s].setRotationalMotor(1);
 				g6dofs[s].setRotationalMotor(2);
 				g6dofs[s].setMaxRotationalMotorForce(0, 10000);
 				g6dofs[s].setMaxRotationalMotorForce(1, 10000);
 				g6dofs[s].setMaxRotationalMotorForce(2, 10000);
+				break;
+				*/
+				break;
 				default:
-				g6dofs[s].setAngularLimit(zeroVec3, zeroVec3);
-				g6dofs[s].setLinearLimit (zeroVec3, zeroVec3);
+				g6dofs[s].setAngularLimit(Vector3f(0.0,0.0,0.0), Vector3f(0.01,0.01,0.01));
+				g6dofs[s].setLinearLimit (Vector3f(0.0,0.0,0.0), Vector3f(0.01,0.01,0.01));
+				break;
 			}
 
 		}
